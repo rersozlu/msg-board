3814c7f79e43023d30aa2183b8cef7ec8e8f1094
rersozlu <ersozlu.rafi@gmail.com>
2022-05-07 19:37:04 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { checkIfWalletConnected, connectWallet } from "./utils/walletFuncs";
+import { getWaveCount, wave, getAllWaves } from "./utils/contractFuncs";
+function App() {
+  const [account, setAccount] = useState("");
+  const [waveNum, setWaveNum] = useState(0);
+  const [message, setMessage] = useState("");
+  const [allMessages, setAllMessages] = useState([]);
+  useEffect(() => {
+    async function checkIfConnected() {
+      const userAcc = await checkIfWalletConnected();
+      setAccount(userAcc);
+      const num = await getWaveCount();
+      setWaveNum(num);
+      const allMessages = await getAllWaves();
+      setAllMessages(allMessages);
+    }
+
+    checkIfConnected();
+  }, []);
+  const allMessagesArray = allMessages.map((msg, index) => {
+    return (
+      <div className="message" key={index}>
+        <h4>MSG NO: {index + 1}</h4>
+        <p>Sender: {msg.address}</p>
+        <p>Content: {msg.message}</p>
+        <p>Date: {msg.timestamp.toString()}</p>
+      </div>
+    );
+  });
+  return (
+    <>
+      <h1 className="warning">
+        PAGE IS NOT RESPONSIVE PLEASE USE SOMETHING WITH A BIGGER SCREEN
+      </h1>
+      <div className="App">
+        <nav>
+          <h1>MESSAGE BOARD</h1>
+          <p>Total Messages: {waveNum}</p>
+          <button onClick={async () => setAccount(await connectWallet())}>
+            ConnectWallet
+          </button>
+        </nav>
+        <div className="form">
+          <input
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+            type="text"
+          />
+          <button onClick={async () => setWaveNum(await wave(message))}>
+            Send Message
+          </button>
+        </div>
+
+        {allMessagesArray}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/utils/contractFuncs.js
@@ -0,0 +1,60 @@
+import { ethers } from "ethers";
+import abi from "./WavePortal.json";
+
+async function getWaveCount() {
+  const { ethereum } = window;
+  const contractAddress = "0xe41E99D39E84Ec080244E1aA5aB15E1b1d708618";
+  const contractABI = abi.abi;
+  if (ethereum) {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const wavePortalContract = new ethers.Contract(
+      contractAddress,
+      contractABI,
+      signer
+    );
+    let count = await wavePortalContract.getTotalWaves();
+    console.log(count.toNumber());
+    return count.toNumber();
+  }
+}
+async function wave(message) {
+  const { ethereum } = window;
+  const contractAddress = "0xe41E99D39E84Ec080244E1aA5aB15E1b1d708618";
+  const contractABI = abi.abi;
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const wavePortalContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
+  let waveTxn = await wavePortalContract.wave(message);
+  console.log("txn mining");
+  await waveTxn.wait();
+  console.log("txn mined", waveTxn.hash);
+  return await getWaveCount();
+}
+
+async function getAllWaves() {
+  const { ethereum } = window;
+  const contractAddress = "0xe41E99D39E84Ec080244E1aA5aB15E1b1d708618";
+  const contractABI = abi.abi;
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const wavePortalContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
+  let waveTxn = await wavePortalContract.getAllWaves();
+  let wavesCleaned = waveTxn.map((msg) => ({
+    address: msg.waver,
+    timestamp: new Date(msg.timestamp * 1000),
+    message: msg.message,
+  }));
+  console.log(wavesCleaned);
+  return wavesCleaned;
+}
+
+export { getWaveCount, wave, getAllWaves };
